1880bd4e6ebe60c31b01bf1a543cadc0f903e88f
karthik-vishnu <vishnukarthi40@gmail.com>
2015-10-27 11:51:38 +0530

    Updated
    local cache done, extra devices added

---
+++ content.js
@@ -0,0 +1,12 @@
+var port = chrome.runtime.connect();
+
+window.addEventListener("message", function(event) {
+  // We only accept messages from ourselves
+  if (event.source != window)
+    return;
+
+  if (event.data.type && (event.data.type == "FROM_PAGE")) {
+    console.log("Content script received: " + event.data.text);
+    port.postMessage(event.data.text);
+  }
+}, false);
